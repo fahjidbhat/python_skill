f8fbf939e62bd9bf0214479d9b818dcb7fe34a31
fahjidbhat <bhatfahjid@gmail.com>
2024-12-09 20:41:07 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+# a=np.array([1,2,3,4])
+# a=np.append(a, [5])
+# a=np.delete(a,3)
+# print(a)
+
+# arr=np.array([[1,2],[3,4]])
+# arr=np.max(arr)
+# print(arr)
+
+
+# a=([1,2,3])
+# series=pd.Series(a,index=['a','b','c'])
+# print(series)
+
+
+f=open("C:\Users\hp\main.class",'r')
