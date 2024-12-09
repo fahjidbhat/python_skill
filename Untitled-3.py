a227cf4c172fa401209370deae8158d662470c15
Fahjid Bhat <bhatfahjid@gmail.com>
2024-12-10 00:04:13 +0530

    Add files via upload

---
+++ Untitled-3.py
@@ -0,0 +1,37 @@
+#numpy/pandas
+import numpy as np
+import pandas as pd
+
+# arr = np.array([1, 2, 3, 4, 5])
+# print(arr)
+
+
+# arr = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+# print(arr)
+# print(arr.sort())
+# print(arr.sum())
+
+# arr = np.array([1, 2, 3, 4])
+# print(arr[0])
+
+# arr = np.array([1, 2, 3, 4])
+# print(arr[2] + arr[3])
+
+# arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+# print(arr[0, 1, 2])
+
+# arr = np.array([1, 2, 3, 4, 5, 6, 7])
+# print(arr[1:5])
+
+# arr = np.array([1, 2, 3, 4, 5, 6, 7])
+# print(arr[-3:-1])
+
+# a = [1, 3, 2]
+# x = pd.Series(a)
+# print(x)
+
+# a = [1, 3, 2]
+# label = pd.Series(a, index = ["a", "b", "c"])
+# print(label)
+# print(label[0])
+# print(label["b"])
